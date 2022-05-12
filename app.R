f7e1dde553d442f22db8645ec9ace44ee034636e
Nikita Tomar <98619645+ntomar55@users.noreply.github.com>
2022-05-12 08:50:24 +0530

    Update app.R

--- app.R
+++ app.R
@@ -1,7 +1,7 @@
-## Author: Taylor Falk
-## tfalk@bu.edu
+## Author: Nikita Tomar
+## ntomar@bu.edu
 ## BU BF591
-## Assignment 7
+## Final Project
 
 # Welcome to R Shiny. All that glitters is not gold.
 library(shiny)
@@ -13,6 +13,9 @@ library(ggrepel)
 library(genefilter)
 library(DT)
 library(tidyverse)
+library(ComplexHeatmap)
+library(beeswarm)
+library(ggbeeswarm)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -77,7 +80,9 @@ ui <- fluidPage(
                 mainPanel(
                     tabsetPanel(
                         tabPanel("Summary", 
-                                 textOutput("c2_sample_text"),
+                                 textOutput("c2_sample_text1"),
+                                 # textOutput("c2_sample_text2"),
+                                 # textOutput("c2_sample_text3"),
                                  tableOutput("c2_table")),
                         tabPanel("Scatter Plot",
                                  mainPanel(
@@ -107,13 +112,13 @@ ui <- fluidPage(
                                     radioButtons("c2_radio_pc2", "Select Principal Component (Y)",
                                                  choices = c(1,2,3,4,5,6,7),
                                                  selected = 2),
-                                    sliderInput(inputId = "c2_slider_pca", min = 1, max = 7,
-                                                label = "Select number of top components for beeswarm plot",
-                                                value = 5, step = 1),
+                                    # sliderInput(inputId = "c2_slider_pca", min = 1, max = 7,
+                                    #             label = "Select number of top components for beeswarm plot",
+                                    #             value = 5, step = 1),
                                 ),
                                 mainPanel(
                                     plotOutput("c2_scatter_pca"),
-                                    # plotOutput("c2_beeswarm_pca")
+                                    plotOutput("c2_beeswarm_pca")
                                 )
                             ),
                         )
@@ -139,7 +144,7 @@ ui <- fluidPage(
                             "Analyses",
                             sidebarLayout(
                                 sidebarPanel(
-                                    HTML(paste(rep("<p>A volcano plot can be generated with <b>'log<sub>2</sub> fold-change'</b> on the x-axis and <b>'p-adjusted'</b> on the y-axis.</p>"),
+                                    HTML(paste(rep("<p>A volcano plot can be generated</p>"),
                                                collapse = "")),
                                     
                                     radioButtons("c3_radio_1", "Choose the column for the x-axis",
@@ -161,8 +166,7 @@ ui <- fluidPage(
                                 
                                 mainPanel(
                                     tabsetPanel(
-                                        tabPanel("Plot", plotOutput("c3_volcano_plot")),
-                                        tabPanel("Table", tableOutput("c3_data_table"))
+                                        tabPanel("Plot", plotOutput("c3_volcano_plot"))
                                     )
                                 ) # end of mainPanel
                                 
@@ -178,7 +182,7 @@ ui <- fluidPage(
                 sidebarPanel(
                     fileInput("c4_fileinput1", "Load normalized count matrix data",
                               accept = ".csv", placeholder = "norm_count_matrix.csv"),
-                    fileInput("c4_fileinput2", "Load normalized count matrix data",
+                    fileInput("c4_fileinput2", "Load sample information data",
                               accept = ".csv", placeholder = "sample_info.csv"),
                 ),
                 mainPanel(
@@ -373,33 +377,37 @@ server <- function(input, output, session) {
         npass = dim(m2)[1]
         nfail = ngene - npass
         tbl = data.frame(X=c('Total', 'Pass', 'Fail'), Genes=c(ngene, npass, nfail),'Percentage (%)'=c(100, round(npass/ngene,2), round(nfail/ngene,2)))
-        output$c2_sample_text <- renderText({ 
-            paste(paste(c("Total samples in dataset ", dim(m)[2]),collapse=""),
-                  paste(c("Filter criteria 1: Genes with at least ", c2.slider_ptile()/100, " percentile variance"),collapse=""), 
-                  paste(c("Filter criteria 2: Genes with at least ", c2.slider_nz_sampl(), " non-zero samples"), collapse=""), collapse='\n')
+        output$c2_sample_text1 <- renderText({ 
+          paste(c("Total samples in dataset ", dim(m)[2]),collapse="")
         })
+        # output$c2_sample_text2 <- renderText({ 
+        #   paste(c("Filter criteria 1: Genes with at least ", c2.slider_ptile()/100, " percentile variance"),collapse="")
+        # })
+        # output$c2_sample_text3 <- renderText({ 
+        #   paste(c("Filter criteria 2: Genes with at least ", c2.slider_nz_sampl(), " non-zero samples"), collapse="")
+        # })
         return(tbl)
     })
     output$c2_scatter1 <- renderPlot({
         datam = c2.load_data()
         datam = c2_modify_data(datam, c2.slider_ptile()/100, c2.slider_nz_sampl())
-        plot <- ggplot(data = datam, aes(x = log(Median+1), y=NumZero)) +
-            geom_point(aes(color = Pass)) +
-            labs( color = c('Filtered')) +
-            theme(legend.position = "bottom") +
-            scale_color_manual(values = c("black", "red")) +
-            theme_minimal()
+        plot <- ggplot(data = datam, aes(x = log(Median+1), y=log(RowVar))) +
+          geom_point(aes(color = Pass)) +
+          labs( color = c('Filtered')) +
+          theme(legend.position = "bottom") +
+          scale_color_manual(values = c("black", "red")) +
+          theme_minimal()
         return(plot)
     })
     output$c2_scatter2 <- renderPlot({
         datam = c2.load_data()
         datam = c2_modify_data(datam, c2.slider_ptile()/100, c2.slider_nz_sampl())
-        plot <- ggplot(data = datam, aes(x = log(Median+1), y=log(RowVar))) +
-            geom_point(aes(color = Pass)) +
-            labs( color = c('Filtered')) +
-            theme(legend.position = "bottom") +
-            scale_color_manual(values = c("black", "red")) +
-            theme_minimal()
+        plot <- ggplot(data = datam, aes(x = log(Median+1), y=NumZero)) +
+          geom_point(aes(color = Pass)) +
+          labs( color = c('Filtered')) +
+          theme(legend.position = "bottom") +
+          scale_color_manual(values = c("black", "red")) +
+          theme_minimal()
         return(plot)
     })
     output$c2_heatmap <- renderPlot({
@@ -420,11 +428,11 @@ server <- function(input, output, session) {
         datam <- c2_modify_data(datam, c2.slider_ptile()/100, c2.slider_nz_sampl())
         filt_data <- datam[datam$Pass=='pass',]
         filt_data <- filt_data[-c(1,2,3,4,5)]
-        print(c2.slider_ptile())
-        print(c2.slider_nz_sampl())
-        print(dim(filt_data))
-        print('filt data')
-        print(head(filt_data))
+        # print(c2.slider_ptile())
+        # print(c2.slider_nz_sampl())
+        # print(dim(filt_data))
+        # print('filt data')
+        # print(head(filt_data))
         if(dim(filt_data)[1]==0 || dim(filt_data)[2]==0) {
             return(NULL)
         }
@@ -445,7 +453,7 @@ server <- function(input, output, session) {
             ggtitle("PCA Scatter Plot") +
             theme(legend.position="bottom") +
             theme_minimal()
-        PCN <- as.integer(c2.slider_pca())
+        # PCN <- as.integer(c2.slider_pca())
         # output$c2_beeswarm_pca <- renderPlot({
         #     bw_plot <- beeswarm(log(pca_df[,1:PCN]+1), pch = 20, col = 2:PCN+1, ylab = "log(PC+1)")
         #     return(bw_plot)
@@ -487,6 +495,7 @@ server <- function(input, output, session) {
     volcano_plot <- function(dataf, x_name, y_name, slider, color1, color2) {
         plot <- ggplot(data = dataf, aes(x = !!sym(x_name), y=-log10(!!sym(y_name)))) +
             geom_point(aes(color = padj< 1*10^(slider))) +
+            scale_color_manual(values = c("FALSE" = color1, "TRUE" = color2)) +
             labs( color = str_glue('{y_name} 1 x 10^ {slider}')) +
             theme(legend.position = "bottom") +
             theme_minimal()
@@ -507,10 +516,6 @@ server <- function(input, output, session) {
         volcano_plot(dataf = c3.load_data(), slider = c3.slider_padj(), 
                      x_name=c3.radio_1(), y_name=c3.radio_2(), 
                      color1=c3.col_1(), color2=c3.col_2())
-    }) # replace this NULL
-    # Same here, just return the table as you want to see it in the web page
-    output$c3_data_table <- renderTable({
-        draw_table(dataf = c3.load_data(), slider = c3.slider_padj())
     }) # replace this NULL
     genetext = " "
     c4.load_data1 <- reactive({
@@ -539,7 +544,7 @@ server <- function(input, output, session) {
             data <- read.csv(file$datapath, header= TRUE, sep=",")
         }
         observe({ #for histogram
-            dataf <- data_info %>%
+            dataf <- data %>%
                 select_if(function(x) ((length(unique(x)) != length(x)) && (length(unique(x)) > 1) && (length(unique(x)) < 5)
                 ))
             choices <- colnames(dataf) 
@@ -588,7 +593,7 @@ server <- function(input, output, session) {
                 theme_minimal()
         } else if(type=="beeswarm plot") {
             plot <- ggplot(dat, aes_string(x = colname, y = "GE_Values", color = colname)) +
-                geom_beeswarm(cex = 3) +
+                geom_beeswarm(cex = 3, aes(size=1)) +
                 theme_minimal()
         }
         return(plot+theme(legend.position="bottom"))
@@ -598,7 +603,12 @@ server <- function(input, output, session) {
         data_info <- c4.load_data2()
         gene <- input$c4_search_gene
         genelist <- data_cnt[,1]
-        if(gene=="" || gene %in% genelist == FALSE) {
+        if(gene=="") {
+          output$c4_error <- renderText({
+            paste(c("Please enter a gene name and click on Plot"))
+          })
+          return(NULL)
+        } else if(gene %in% genelist == FALSE) {
             output$c4_error <- renderText({
                 paste(c("The input gene value ",
                         input$c4_search_gene, 
